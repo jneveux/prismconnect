ed479d6b8387fe0b3e19a2899b353d3b370dccbc
Jean Neveux <jean.neveux@gmail.com>
2017-07-16 23:17:45 +0100

    Record response body

--- connectprism/index.js
+++ connectprism/index.js
@@ -1,31 +1,3 @@
-/**
- * Created by jean on 7/16/17.
- */
-// var prism = require('connect-prism');
-//
-// prismMode = 'proxy';
-//
-// prism.create({
-//   name: 'serve',
-//   mode: prismMode,
-//   context: '/api',
-//   host: 'localhost',
-//   port: 9090,
-//   delay: 'auto',
-//   rewrite: {
-//     '^/api/': '/api/',
-//   },
-//   proxyConfig: {
-//     options: {
-//       target: 'http://localhost:3000', // absolute URL as defined by config https, host, port, and context, override at your own risk
-//       xfwd: false, // don't add x-forward headers
-//       secure: false, // don't verify SSL certs
-//       prependPath: false // don't prepend path to target context when proxying
-//     },
-//     onProxyCreated: function (q,a) {}
-//   }
-// });
-
 var httpProxy = require('http-proxy');
 
 var options = {
@@ -35,6 +7,31 @@ var options = {
 
 
 var proxy = httpProxy.createProxyServer(options); // See (†)
+
+proxy.on('proxyRes', function (proxyRes, req, res) {
+
+  // collect response data
+  var proxyResData='';
+  proxyRes.on('data', function (chunk) {
+    proxyResData +=chunk;
+  });
+  proxyRes.on('end',function () {
+
+    var snifferData =
+      {
+        request:{
+          data:req.body,
+          headers:req.headers,
+          url:req.url,
+          method:req.method},
+        response:{
+          data:proxyResData,
+          headers:proxyRes.headers,
+          statusCode:proxyRes.statusCode}
+      };
+    console.log(snifferData.response.data);
+  });
+});
 
 proxy.listen(3001);
 
