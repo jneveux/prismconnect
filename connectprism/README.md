35df9125f27caada86c36dee0fd7de33f40d77cb
Jean Neveux <jean.neveux@gmail.com>
2017-07-16 22:38:28 +0100

    Test alternative to connect-prism

---
+++ connectprism/README.md
@@ -0,0 +1,2 @@
+# Proxy for recording JSON
+* test with [http-proxy](https://github.com/nodejitsu/node-http-proxy)
--- connectprism/index.js
+++ connectprism/index.js
@@ -1,28 +1,41 @@
 /**
  * Created by jean on 7/16/17.
  */
-var prism = require('connect-prism');
+// var prism = require('connect-prism');
+//
+// prismMode = 'proxy';
+//
+// prism.create({
+//   name: 'serve',
+//   mode: prismMode,
+//   context: '/api',
+//   host: 'localhost',
+//   port: 9090,
+//   delay: 'auto',
+//   rewrite: {
+//     '^/api/': '/api/',
+//   },
+//   proxyConfig: {
+//     options: {
+//       target: 'http://localhost:3000', // absolute URL as defined by config https, host, port, and context, override at your own risk
+//       xfwd: false, // don't add x-forward headers
+//       secure: false, // don't verify SSL certs
+//       prependPath: false // don't prepend path to target context when proxying
+//     },
+//     onProxyCreated: function (q,a) {}
+//   }
+// });
 
-prismMode = 'proxy';
+var httpProxy = require('http-proxy');
 
-prism.create({
-  name: 'serve',
-  mode: prismMode,
-  context: '/api',
-  host: 'localhost',
-  port: 9090,
-  delay: 'auto',
-  rewrite: {
-    '^/api/': '/api/',
-  },
-  proxyConfig: {
-    options: {
-      target: 'http://localhost:3000', // absolute URL as defined by config https, host, port, and context, override at your own risk
-      xfwd: false, // don't add x-forward headers
-      secure: false, // don't verify SSL certs
-      prependPath: false // don't prepend path to target context when proxying
-    },
-    onProxyCreated: function (q,a) {}
-  }
-});
+var options = {
+  "target": "http://localhost:3000",
+  "secure": false
+};
+
+
+var proxy = httpProxy.createProxyServer(options); // See (†)
+
+proxy.listen(3001);
+
 
